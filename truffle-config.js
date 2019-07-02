c9e8a43b6b382a164f74452815d527d6f95f82c5
yehia67 <yehiatarek67@gmail.com>
2019-07-02 10:12:02 +0200

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,35 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+module.exports = {
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*', // eslint-disable-line camelcase
+        },
+        ganache: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*', // eslint-disable-line camelcase
+        },
+        ropsten: {
+            provider: function() {
+                return new HDWalletProvider(
+                    process.env.MNEMONIC,
+                    `https://ropsten.infura.io/${process.env.INFURA_API_KEY}`
+                )
+            },
+            gas: 5000000,
+            gasPrice: 25000000000,
+            network_id: 3
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
+}
