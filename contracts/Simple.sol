791185f85f48c0915355dd965ed50161745035c1
yehia67 <yehiatarek67@gmail.com>
2019-07-05 09:21:06 +0200

    write less code

---
+++ contracts/Simple.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Simple{
+string hello ="hello world!";    
+function get() public view returns(string memory){
+return hello;
+}
+function set(string memory _hello) public{
+hello = _hello;
+}
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var TodoList = artifacts.require("./TodoList.sol");
+var Simple = artifacts.require("./Simple.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TodoList);
+    deployer.deploy(Simple);
 };
